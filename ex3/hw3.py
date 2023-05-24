615b6ad4a251f42e7f4479d112595dda26ff0fd2
albertboronov <albertboronov@gmail.com>
2023-05-24 19:15:04 +0700

    the lust of homework

---
+++ ex3/hw3.py
@@ -0,0 +1,14 @@
+a=(input( 'input six-digit number'))
+b=int(a[0])
+c=int(a[1])
+d=int(a[2])
+e=b+c+d
+b1=int(a[3])
+c1=int(a[4])
+d1=int(a[5])
+e1=b1+c1+d1
+if (e==e1) :
+    print('its a lucky ticket')
+else :
+    print('its a unlucky ticket')
+    
---
+++ ex4/hw4.py
@@ -0,0 +1,9 @@
+a=  int(input('введите шириу шоколадки '))
+b=  int(input('введите длинну шоколадки '))
+c=  int(input('сколько дoлек надо'))
+if (c%b==0):
+    print('yes')
+elif(c%a==0):
+     print('yes')
+else:
+    print('no')
